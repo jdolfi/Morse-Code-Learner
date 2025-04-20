8e0ed588a5c13562e687433e04e621441f416c4f
jdolfi <90691505+jdolfi@users.noreply.github.com>
2025-04-20 05:39:42 -0400

    Add files via upload
    Wont compile without these corrections.

---
+++ EEProm.h
@@ -0,0 +1,46 @@
+#ifndef EEPROM_H
+#define EEPROM_H
+
+// much straight from the datasheet
+
+typedef unsigned char BYTE;
+typedef unsigned int WORD;
+
+void IapIdle();
+BYTE IapReadByte(WORD addr);
+
+//-----------------------------------------------
+#define CMD_IDLE 0		//Stand-By
+#define CMD_READ 1		//IAP Byte-Read
+#define CMD_PROGRAM 2	//IAP Byte-Program
+#define CMD_ERASE 3		//IAP Sector-Erase
+
+//#define ENABLE_IAP 0x80 //if SYSCLK<30MHz
+//#define ENABLE_IAP 0x81 //if SYSCLK<24MHz
+//#define ENABLE_IAP 0x82 //if SYSCLK<20MHz
+//#define ENABLE_IAP 0x83 //if SYSCLK<12MHz
+#define ENABLE_IAP 0x84 //if SYSCLK<6MHz
+//#define ENABLE_IAP 0x85 //if SYSCLK<3MHz
+//#define ENABLE_IAP 0x86 //if SYSCLK<2MHz
+//#define ENABLE_IAP 0x87 //if SYSCLK<1MHz
+//Start address for STC15 series MCU EEPROM
+
+// end of datasheet~ish code
+
+#define SETTINGS_ADDRESS	0x0400
+#define MACRO0_ADDRESS		0x0600
+#define MACRO1_ADDRESS		0x0800
+#define MACRO2_ADDRESS		0x0A00
+#define MACRO3_ADDRESS		0x0C00
+
+void IapIdle();
+BYTE IapReadByte(WORD addr);
+void IapProgramByte(WORD addr, BYTE dat);
+void IapEraseSector(WORD addr);
+
+void LoadNvSettingsFromEeprom();
+void StoreNvSettingsToEeprom();
+void LoadNvMacroFromEeprom(unsigned char macroNum);
+void StoreNvMacroToEeprom(unsigned char macroNum);
+
+#endif
---
+++ HwAbstraction.h
@@ -0,0 +1,59 @@
+typedef enum
+{
+	LED_OFF		= 0x00,
+	LED_RED		= 0x01,
+	LED_GREEN	= 0x02,
+	LED_MIX		= 0x03,
+	
+} LED_COLOR_ENUM ;
+
+typedef enum
+{
+
+	TONE_OUT_EAR0  = 0x00,
+	TONE_OUT_EAR1  = 0x01,
+	TONE_OUT_EAR2  = 0x02,
+	TONE_OUT_EAR3  = 0x03,
+	TONE_OUT_EAR4  = 0x04,
+	TONE_OUT_EAR5  = 0x05,
+	TONE_OUT_EAR6  = 0x06,
+	TONE_OUT_EAR7  = 0x07,
+	TONE_OUT_SPKR1 = 0x10,
+	TONE_OUT_SPKR2 = 0x20,
+	TONE_OUT_SPKR3 = 0x30,
+	TONE_OUT_OFF   = 0xFF,
+	
+} TONE_OUT_ENUM ;
+
+typedef enum
+{
+	NO_BUTTON   = 0x00 ,
+	PWR_BUTTON  = 0x01,
+	HF_BUTTON   = 0x02,
+	TONE_BUTTON = 0x04,
+	VOL_BUTTON  = 0x08,
+	
+} BUTTON_ENUM ;
+
+typedef enum
+{
+	DIT_PRESSED  = 0x01,
+	DAH_PRESSED  = 0x02,
+	BOTH_PRESSED = 0x03,
+	
+} KEYS_PRESSED_ENUM ;
+
+void HwInit();
+void HwGlobalIntEnable();
+void HwGlobalIntDisable();
+void HwStartToneTimer(unsigned short timerValue);
+void HwStopToneTimer();
+void HwRestartUnitTimer(unsigned short timerValue);
+void HwSetHfLed(unsigned char on);
+void HwSetUvLed(LED_COLOR_ENUM ledMode);
+void HwKeyerOut(unsigned char keyerOn);
+unsigned char HwKeysPressed();
+unsigned char HwButtonsPressed();
+void SetSpeakerVolume(char pVol);
+void HwSpkrMgr();
+
